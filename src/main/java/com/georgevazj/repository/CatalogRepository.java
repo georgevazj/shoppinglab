f07d0e3fc12b316ad2821de776b414116353be39
Jorge <georgevazj@gmail.com>
2017-05-09 15:44:20 +0200

    Repositorio para el catalogo de productos

--- src/main/java/com/georgevazj/repository/CatalogRepository.java
+++ src/main/java/com/georgevazj/repository/CatalogRepository.java
@@ -1,6 +1,8 @@
 package com.georgevazj.repository;
 
 import com.georgevazj.entity.Catalog;
+import com.georgevazj.entity.Item;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -9,8 +11,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 
 @RepositoryRestResource(collectionResourceRel = "catalog", path = "catalog")
-public interface CatalogRepository {
-
+public interface CatalogRepository extends MongoRepository<Catalog, String> {
     public Catalog findByName(@Param("name") String name);
-
 }
